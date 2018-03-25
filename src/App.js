d4d2bbc2a179150b4820a52d50477e71097129c3
kbefff <DeKkH7283>
2018-03-24 22:45:22 -0700

    home and about navigation functioning

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Home from './components/Home.js';
+import About from './components/About.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
 
-export default App;
+
+
+// const Topic = ({ match }) => (
+//   <div>
+//     <h3>{match.params.topicId}</h3>
+//   </div>
+// )
+
+// const Topics = ({ match }) => (
+//   <div>
+//     <h2>Topics</h2>
+//     <ul>
+//       <li>
+//         <Link to={`${match.url}/rendering`}>
+//           Rendering with React
+//         </Link>
+//       </li>
+//       <li>
+//         <Link to={`${match.url}/components`}>
+//           Components
+//         </Link>
+//       </li>
+//       <li>
+//         <Link to={`${match.url}/props-v-state`}>
+//           Props v. State
+//         </Link>
+//       </li>
+//     </ul>
+
+//     <Route path={`${match.path}/:topicId`} component={Topic}/>
+//     <Route exact path={match.path} render={() => (
+//       <h3>Please select a topic.</h3>
+//     )}/>
+//   </div>
+// )
+
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/components/Home.js">Home</Link></li>
+        <li><Link to="/components/About.js">About</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/components/Home.js" component={Home}/>
+      <Route exact path="/components/About.js" component={About}/>
+    </div>
+  </Router>
+)
+export default App
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+const About = () => (
+    <div>
+      <h2>This is the About section</h2>
+    </div>
+  )
+
+
+export default About
