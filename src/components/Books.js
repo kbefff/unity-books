845b36d8652501483b47e0bf967db90e332a63cb
kbefff <DeKkH7283>
2018-03-25 19:11:12 -0700

    books content complete

--- src/components/Books.js
+++ src/components/Books.js
@@ -8,6 +8,68 @@ const Books = () => (
     <div>
       <Navigation />
       <h2>Books</h2>
+
+        <h4>Alcoholics Anonymous</h4>
+          <li>Big Book</li>
+          <li>Came to Believe</li>
+          <li>12 x 12</li>
+          <li>Living Sober</li>
+          <li>Daily Meditations</li>
+          <li>Grapevine</li>
+          <li>Language of the Heart</li>
+
+
+        <h4>Anorexics and Bulimics Anonymous</h4>
+          <li>ABA: Recovery for Anorexia and Bulimia</li>
+
+
+
+        <h4>Debters Anonymous</h4>
+
+
+        <h4>Gamblers Anonymous</h4>
+          <li>Big Book: Sharing Recovery through GA</li>
+          <li>Red Book: A New Beginning</li>
+
+
+        <h4>Hazelden</h4>
+          <li>Blackout Girl: Growing Up + Drying out in America</li>
+          <li>Diary of an Alcoholic Housewife</li>
+          <li>Drop the Rock</li>
+          <li>Food for Thought: Daily Meditations for Overeaters</li>
+          <li>Mrs. Marty Mann: The First Lady of AA</li>
+          <li>Quitting Meth</li>
+          <li>Meth: The Basics</li>
+          <li>Secret Scars: Uncovering + Understanding the Addiction of Self-Injury</li>
+          <li>Taking Back Your Life: Women + Problem Gambling</li>
+
+
+        <h4>Melody Beattie</h4>
+          <li>The New Codependency</li>
+          <li>Gratitude</li>
+
+
+        <h4>Narcotics Anonymous</h4>
+          <li>Basic Text 6th Ed.</li>
+          <li>Just for Today</li>
+
+
+        <h4>Nicotine Anonymous</h4>
+          <li>Basic Text 4th Ed.</li>
+          <li>A Year of Miracles: a meditation for every day</li>
+          <li>Our Path to Freedom: 12 stories of Recovery</li>
+          <li>To the Dipper + Chewer</li>
+
+        <h4>Overeaters Anonymous</h4>
+          <li>Abstinence</li>
+          <li>A New Beginning</li>
+          <li>Seeking the Spiritual Path</li>
+          <li>I Put My Hand in Yours</li>
+          <li>Lifeline Sampler</li>
+          <li>For Today</li>
+          <li>Voices of Recovery</li>
+          <li>12 Steps & 12 Traditions</li>
+          <li>Local Author Amy Hatvany: Best Kept Secret (coming fall 2011)</li>
     </div>
   )
 
